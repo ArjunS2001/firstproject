70ab97f3dfe36388028d9308011dd63f77b53eff
Arjun S <84306434+ArjunS2001@users.noreply.github.com>
2021-05-17 20:27:15 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,12 @@
+function purple__button()
+{
+ document.body.style.background="purple"
+}
+function red__button()
+{
+    document.body.style.backgroundColor="red"
+}
+function green__button()
+{
+    document.body.style.backgroundColor="green"
+}
